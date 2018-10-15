45e2435d9f5bba864ba7648eb00973422f38b75d
KevinMAnderson <kma2k4@hotmail.com>
2018-10-14 19:19:23 -0600

    ProjectParallax section background images are now dynamic...still need to style the overlay properly on hover

--- src/Components/Common/Project/ProjectParallax.js
+++ src/Components/Common/Project/ProjectParallax.js
@@ -12,31 +12,27 @@ import {
     SarticleMainHeading
 } from "../../../shared/StyledComponents";
 
-let sectionBgImg;
+const renderBgSections = bannerImg => {
+    return bannerImg.map((img, index) => {
+        const { overLay, src } = img;
+
+        const style = {
+            backgroundImage: "url(" + require(`../../../${src}`) + ")"
+        };
+
+        return (
+            <Ssection key={index} style={style}>
+                <Soverlay>{overLay}</Soverlay>
+            </Ssection>
+        );
+    });
+};
 
 const ProjectParallax = ({ project }) => {
     const { bannerImg } = content.projects[project];
     // TODO: can't get section background dynamic this way
-    // can I use a picture element in lieu but still
-    // get the parallax effect?
-    const renderBgSections = () => {
-        return bannerImg.map((img, index) => {
-            const { overLay, src } = img;
 
-            sectionBgImg = `../../../${src}`;
-            // const style = {
-            //     backgroundImage: `url(../../../${src})`
-            // };
-
-            return (
-                <Ssection key={index}>
-                    <Soverlay>{overLay}</Soverlay>
-                </Ssection>
-            );
-        });
-    };
-
-    return <Fragment>{renderBgSections()}</Fragment>;
+    return <Fragment>{renderBgSections(bannerImg)}</Fragment>;
 };
 
 export default ProjectParallax;
@@ -45,16 +41,16 @@ export default ProjectParallax;
 
 const Ssection = styled(SsectionParallax)`
     height: 95vh;
-    background-image: url(${sectionBgImg});
+    background-image: url();
 `;
 
 const Soverlay = styled(SarticleMainHeading)`
     opacity: 0;
-    &:hover {
-        transition: all 1s ease-in;
+    transition: all 1s ease-in;
+    ${Ssection}:hover & {
         opacity: 1;
     }
-    ${mediaMin.desktop` 
+    ${mediaMin.desktop`
     margin: auto;
     text-align: center;`};
 `;
